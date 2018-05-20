92c6edbc687f690d23a588ff58ded924d6d37171
Daniel <danieljab7@gmail.com>
2018-05-20 00:21:44 -0400

    Added settings page and firebase sign out capability

---
+++ MainScreens/SettingsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SettingsViewController.swift
+//  GymApp
+//
+//  Created by Daniel Jabbour on 2018-05-20.
+//  Copyright © 2018 Daniel Jabbour. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func signOutAction(_ sender: Any) {
+        
+        //Method for signing users out
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                present(vc, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
