4689803abcd0def343615346a73533160a39f915
Daniel <danieljab7@gmail.com>
2018-05-21 22:21:52 -0400

    Created workout class and developed table view UI

---
+++ GymApp/workout.swift
@@ -0,0 +1,30 @@
+//
+//  workout.swift
+//  GymApp
+//
+//  Created by Daniel Jabbour on 2018-05-21.
+//  Copyright © 2018 Daniel Jabbour. All rights reserved.
+//
+
+import UIKit
+
+class workout {
+    
+    //workout class properties
+    var title: String
+    var reps: Int
+    var sets: Int
+    
+    //Constructor
+    init(title: String, reps: Int, sets: Int){
+        
+        //Add error handling for false values
+        
+        self.title = title
+        self.reps = reps
+        self.sets = sets
+        
+    }
+    
+    
+}
