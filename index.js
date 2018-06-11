b4d5534fc203fca14f698361e4a347c0403a0863
danieljabbour <jabbourd@mcmaster.ca>
2018-06-11 10:14:48 -0400

    Added page routing and image source

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Login from './src/components/Login/Login';
+
 
 AppRegistry.registerComponent('GymApp', () => App);
---
+++ src/components/Login/Login.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Image, Text} from 'react-native';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          {/*<Image
+            style = {styles.logo}
+            source = {require("image URL")}
+          />*/}
+          <Text style={styles.title}>A fitness analytics application</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2c3e50'
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100
+  },
+  title: {
+    color: '#FFF',
+    marginTop: 10,
+    width: 160,
+    textAlign: 'center',
+  }
+});
