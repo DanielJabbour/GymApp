748edea5bd76000fd3c643a96bcf715a3c553a76
danieljabbour <jabbourd@mcmaster.ca>
2018-06-11 12:22:54 -0400

    Updated documentation for app.js

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Login from './src/components/Login/Login';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/Login/Login'; //Importing login page into app
 
 type Props = {};
+
 export default class App extends Component<Props> {
   render() {
     return (
