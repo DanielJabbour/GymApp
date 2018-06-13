2794ebe8e45aa8365da5be76300c3b4f44d0c74a
danieljabbour <jabbourd@mcmaster.ca>
2018-06-12 23:46:25 -0400

    Build updates

---
+++ Screens/Main/Settings.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+
+export default class Settings extends React.Component {
+    render() {
+        return(
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#2c3e50',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  })
