c987d3304a7bdb233058f2b9f18df4cea2da8afd
danieljabbour <jabbourd@mcmaster.ca>
2018-06-22 23:19:08 -0400

    Migrated back to swift dev, react and flutter were cool

---
+++ ReactNative/App.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import ScreenStack from './Navigation/ScreenStack'
+
+const App = () => <ScreenStack />;
+
+export default App
