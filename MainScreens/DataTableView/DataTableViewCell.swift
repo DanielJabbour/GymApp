a5e74f90a5e6ebf7f9781655ae2df201f411d685
danieljabbour <jabbourd@mcmaster.ca>
2018-08-02 19:11:44 -0400

    Changed Data view to a UITableViewController for better summary of data

---
+++ MainScreens/DataTableView/DataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DataTableViewCell.swift
+//  GymApp
+//
+//  Created by Daniel Jabbour on 2018-08-02.
+//  Copyright © 2018 Daniel Jabbour. All rights reserved.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
