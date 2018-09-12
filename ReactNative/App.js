ba08d004ee6fb8e5cf3ac9c6119dc87e0d409cbb
Daniel <danieljab7@gmail.com>
2018-09-11 23:18:10 -0400

    Removed old react files

--- ReactNative/App.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import ScreenStack from './Navigation/ScreenStack'
-
-const App = () => <ScreenStack />;
-
-export default App
