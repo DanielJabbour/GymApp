52f67f55882ec75e2a02369b9f77c3546ae57596
Daniel <danieljab7@gmail.com>
2018-09-14 22:09:20 -0400

    Resolved error of data not loading correctly due to processing method taking too long before tableView function is executed

--- MainScreens/DataTableView/DataTableViewController.swift
+++ MainScreens/DataTableView/DataTableViewController.swift
@@ -37,7 +37,6 @@ class DataTableViewController: UITableViewController {
         
         //Method to retrieve muscle group data
         getGroupData()
-
         
     }
 
@@ -63,81 +62,94 @@ class DataTableViewController: UITableViewController {
             fatalError("The dequed cell is not an instance of DataTableViewCell")
         }
         
-        //TO DO: Read data from database to display on charts. X-axis = session #s OR weeks, Y-axis = cumulative score (sets x reps x weight)
-        
-        //Uncomment this line and comment out the lines between MARK1 & MARK2 to change x-axis to dates
-//        cell.lineChart.setLineChartData(xValues: xVals[indexPath.row], yValues: yVals[indexPath.row], label: "")
-        
-        //MARK-1
-        var dataPoint = [ChartDataEntry]()
-        
-        //FINAL ERROR IS HERE
-        for index in 0...yVals.count - 1 {
-            for index2 in 0...yVals[index].count - 1{
-                dataPoint.append(ChartDataEntry(x: Double(index2 + 1), y: Double(yVals[index][index2])))
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(100), execute: {
+            // Put your code which should be executed with a delay here
+            
+            //TO DO: Read data from database to display on charts. X-axis = session #s OR weeks, Y-axis = cumulative score (sets x reps x weight)
+            
+            //Uncomment this line and comment out the lines between MARK1 & MARK2 to change x-axis to dates
+            //        cell.lineChart.setLineChartData(xValues: xVals[indexPath.row], yValues: yVals[indexPath.row], label: "")
+            
+            //MARK-1
+            var dataPoint = [ChartDataEntry]()
+            
+            /* FINAL ERROR IS HERE... experiencing random crashes?
+             Hypothesis: getGroupData() Method hasn't completed running before this call
+             Sequence should be (and is) getGroupData(), then this method.
+             Sleeping until method finishes seems to erradicate this error
+             */
+            
+            //sleep(1)
+//            print("YVAL 1:")
+//            print(self.yVals.count)
+            for index in 0...self.yVals.count - 1 {
+                for index2 in 0...self.yVals[index].count - 1{
+                    dataPoint.append(ChartDataEntry(x: Double(index2 + 1), y: Double(self.yVals[index][index2])))
+                }
+                self.lineDataEntry.append(dataPoint)
+                dataPoint.removeAll()
             }
-            lineDataEntry.append(dataPoint)
-            dataPoint.removeAll()
-        }
-        
-        var muscleGroupsArr = [String]()
-        
-        for (key, _) in self.muscleGroupsOld {
-            let currentMuscleGroup = muscleGroupsOld[key] as! [String:AnyObject]
-            let checkForEmptyDict = currentMuscleGroup["Workouts"] as! [String:AnyObject]
-            if key != "Dummy" && checkForEmptyDict.count > 1 {
-                muscleGroupsArr.append(key)
+            
+            var muscleGroupsArr = [String]()
+            
+            for (key, _) in self.muscleGroupsOld {
+                let currentMuscleGroup = self.muscleGroupsOld[key] as! [String:AnyObject]
+                let checkForEmptyDict = currentMuscleGroup["Workouts"] as! [String:AnyObject]
+                if key != "Dummy" && checkForEmptyDict.count > 1 {
+                    muscleGroupsArr.append(key)
+                }
             }
-        }
-        
-        let chartDataSet = LineChartDataSet(values: lineDataEntry[indexPath.row], label: muscleGroupsArr[indexPath.row])
-        let chartData = LineChartData()
-        chartData.addDataSet(chartDataSet)
-        
-        //Data Set Color Customization
-        chartDataSet.colors = [UIColor.cyan]
-        chartDataSet.setCircleColor(UIColor.cyan) //0, 178, 202
-        chartDataSet.circleHoleColor = UIColor.cyan
-        chartDataSet.circleHoleRadius = 4.0
-        
-        //Color Gradient Definition
-        let gradientColors = [UIColor.cyan.cgColor, UIColor.clear.cgColor] as CFArray
-        let colorLocations: [CGFloat] = [1.0, 0.0]
-        guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("Gradient Error"); return cell}
-        chartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 0.0)
-        chartDataSet.drawFilledEnabled = true
-        chartDataSet.mode = .cubicBezier
-        chartDataSet.cubicIntensity = 0.2
-        chartDataSet.circleRadius = 3.0
-        chartDataSet.valueTextColor = NSUIColor.cyan
-        
-        //MARK-2
-        
-        //All other additions to this function will go here
-        cell.lineChart.xAxis.labelPosition = .bottom
-        cell.lineChart.xAxis.drawGridLinesEnabled = false
-        cell.lineChart.chartDescription?.enabled = false
-        //cell.lineChart.legend.enabled = false -- Uncomment this for disabling the bottom label
-        cell.lineChart.rightAxis.enabled = false
-        cell.lineChart.leftAxis.drawGridLinesEnabled = false
-        cell.lineChart.leftAxis.drawLabelsEnabled = true
-        cell.lineChart.backgroundColor = UIColor.black
-        cell.lineChart.xAxis.labelTextColor = UIColor.cyan //Axis colors
-        cell.lineChart.leftAxis.labelTextColor = UIColor.cyan
-        cell.lineChart.legend.textColor = UIColor.cyan //Legend color
-        
-        cell.lineChart.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
-        
-        //Error is here, but why?
-        cell.lineChart.xAxis.axisMaximum = Double(yVals[indexPath.row].count + 1)
-        cell.lineChart.xAxis.axisMinimum = 1.0
-        cell.lineChart.xAxis.labelCount = yVals[indexPath.row].count
-        
-        //Add data to view
-        cell.lineChart.data = chartData
-        
-        //This must stay at end of function
-        cell.lineChart.notifyDataSetChanged()
+            
+            let chartDataSet = LineChartDataSet(values: self.lineDataEntry[indexPath.row], label: muscleGroupsArr[indexPath.row])
+            let chartData = LineChartData()
+            chartData.addDataSet(chartDataSet)
+            
+            //Data Set Color Customization
+            chartDataSet.colors = [UIColor.cyan]
+            chartDataSet.setCircleColor(UIColor.cyan) //0, 178, 202
+            chartDataSet.circleHoleColor = UIColor.cyan
+            chartDataSet.circleHoleRadius = 4.0
+            
+            //Color Gradient Definition
+            let gradientColors = [UIColor.cyan.cgColor, UIColor.clear.cgColor] as CFArray
+            let colorLocations: [CGFloat] = [1.0, 0.0]
+            guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("Gradient Error"); return}
+            chartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 0.0)
+            chartDataSet.drawFilledEnabled = true
+            chartDataSet.mode = .cubicBezier
+            chartDataSet.cubicIntensity = 0.2
+            chartDataSet.circleRadius = 3.0
+            chartDataSet.valueTextColor = NSUIColor.cyan
+            
+            //MARK-2
+            
+            //All other additions to this function will go here
+            cell.lineChart.xAxis.labelPosition = .bottom
+            cell.lineChart.xAxis.drawGridLinesEnabled = false
+            cell.lineChart.chartDescription?.enabled = false
+            //cell.lineChart.legend.enabled = false -- Uncomment this for disabling the bottom label
+            cell.lineChart.rightAxis.enabled = false
+            cell.lineChart.leftAxis.drawGridLinesEnabled = false
+            cell.lineChart.leftAxis.drawLabelsEnabled = true
+            cell.lineChart.backgroundColor = UIColor.black
+            cell.lineChart.xAxis.labelTextColor = UIColor.cyan //Axis colors
+            cell.lineChart.leftAxis.labelTextColor = UIColor.cyan
+            cell.lineChart.legend.textColor = UIColor.cyan //Legend color
+            
+            cell.lineChart.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
+            
+            //Error is here, but why?
+            cell.lineChart.xAxis.axisMaximum = Double(self.yVals[indexPath.row].count + 1)
+            cell.lineChart.xAxis.axisMinimum = 1.0
+            cell.lineChart.xAxis.labelCount = self.yVals[indexPath.row].count
+            
+            //Add data to view
+            cell.lineChart.data = chartData
+            
+            //This must stay at end of function
+            cell.lineChart.notifyDataSetChanged()
+            
+        })
         
         return cell
     }
@@ -193,16 +205,11 @@ class DataTableViewController: UITableViewController {
                 dataArr.append(data(date: dateVal, value: cumulitiveVal))
                 
                 aggregatePointsDict[dateVal] = (aggregatePointsDict[dateVal] ?? 0) + cumulitiveVal
-                
             }
         }
         
         //Order gets messed up in dict
-        
-        //NEED TO ADD ERROR HANDLING FOR EMPTY WORKOUTS GROUPS IN MUSCLEGROUPSOLD
-        
         if dataArr.count != 0 {
-            //Error is here
             for index in 0...dataArr.count - 1 {
                 let currentDate = dataArr[index].date
                 var currentVal = dataArr[index].value
@@ -221,6 +228,8 @@ class DataTableViewController: UITableViewController {
             //Reverse array to maintain order from earliest to latest date
             self.xVals.append(groupedDataX.reversed())
             self.yVals.append(groupedDataY.reversed())
+//            print("YVAL 2: ")
+//            print(yVals.count)
         }
     }
     
